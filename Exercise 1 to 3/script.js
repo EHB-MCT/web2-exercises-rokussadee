69afcc7050c7ad2411190453d98d0c2465f9e79a
rokussadee <rokus.sadee@student.ehb.be>
2021-10-03 22:35:36 +0200

    Ex. 4-6;

--- Exercise 1 to 3/script.js
+++ Exercise 1 to 3/script.js
@@ -3,45 +3,81 @@
 
 window.onload = function() {
     
-    
     const msgContainer = document.getElementById('message')
     const form = document.getElementById('form')
+    const dishesContainer = document.getElementById('order');
+
+    let dishes = [];
+    dishes.push({
+        id: 1,
+        name: 'Burger and french fries',
+        price: '18'
+    });
+    dishes.push({
+        id: 2,
+        name: 'Spaghetti Bolognaise',
+        price: '16'
+    });
+    dishes.push({
+        id: 3,
+        name: 'Vol au Vent',
+        price: '19'
+    });
+
+    dishes.forEach((dish) => {
+        let htmlString = `<input type="radio" name="options" class="radio" id="${dish.id}" value="${dish.name}"> <label for="${dish.id}">${dish.name}, ${dish.price}</label>`;
+        dishesContainer.insertAdjacentHTML('beforeend', htmlString);
+    });
+
     const inputList = Array.from(document.getElementsByTagName('input'));
 
+    let radioList = Array.from(document.getElementsByClassName('radio'))
+    console.log(radioList);
     
     console.log('loaded')
     
-    form.addEventListener('submit', submit)
-    
-    function submit(e) {
+    form.addEventListener('submit', function(e) {
         e.preventDefault();
-        let data = {
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            order: document.getElementById('order').value
+        inputList.forEach(input => {
+                    
+            if (input.value == null || input.value == '') {
+                input.classList.remove('border-green-600');
+                input.classList.add('border-red-600');
+            } else {
+                input.classList.remove('border-red-600');
+                input.classList.add('border-green-600');
+            }
+        });
+        if (document.getElementById('name').value == '' || document.getElementById('email').value == '' || document.querySelector('input[type="radio"]:checked') == undefined) {
+            msgContainer.innerHTML = `
+            <p>Please enter your data in the <span class="missingClass">missing</span> fields.</p>
+            `;
+        } else {
+            let data = {
+                name: document.getElementById('name').value,
+                email: document.getElementById('email').value,
+                order: document.querySelector('input[type="radio"]:checked').value
+            }
+
+            submit(data);
+        }
+    })
+    
+    function submit(data) {
+
+        let orderDetails = {
+            name: data.name,
+            email: data.email,
+            order: data.order
         }
 
-        msgDisplay(data);
+        printOrder(orderDetails);
     }
 
     
-    function msgDisplay(data) {
-
-        if (data.name == '' || data.email == '' || data.order == '') {
-            msgContainer.innerHTML = `
-            <p>Please enter your data in the required fields.</p>
-            `
-
-            inputList.forEach(e => {
-                e.classList.add('border-red-600');
-            });
-        } else {
-            msgContainer.innerHTML = `
-            <p>The order for the customer <span class="nameClass">${data.name}</span> is the following: <span class="nameClass">${data.order}</span>. The customer may be notified by email: <span class="nameClass">${data.email}</span></p>
-            `
-            inputList.forEach(e => {
-                e.classList.remove('border-red-600');
-            });
-        }
+    function printOrder(data) {
+        msgContainer.innerHTML = `
+        <p>The order for the customer <span class="nameClass">${data.name}</span> is the following: <span class="nameClass">${data.order}</span>. The customer may be notified by email: <span class="nameClass">${data.email}</span></p>
+        `
     }
 }
